046c652f260f6c7d9e5b31c3b373a2f21ce7f09e
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-11-28 14:44:10 +0900

    Set react-router

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import TopPage from 'pages/TopPage';
+import { makeStyles } from '@material-ui/styles';
+import { Box } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '120px auto'
+  }
+}));
+
+export default function () {
+  const classes = useStyles();
+
   return (
-    <TopPage />
+    <BrowserRouter>
+      <Box className={classes.root}>
+        <Switch>
+          <Route path='/' component={TopPage} />
+        </Switch>
+      </Box>
+    </BrowserRouter>
   );
 }
-
-export default App;
