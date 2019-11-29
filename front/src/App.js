442dbb024d3076d7906d5bd25ab316f69f785768
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-11-29 15:56:05 +0900

    Set routing for Signup

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter,
   Route,
+  Redirect,
   Switch,
 } from 'react-router-dom';
-import TopPage from 'pages/TopPage';
 import { makeStyles } from '@material-ui/styles';
 import { Box } from '@material-ui/core';
+import TopPage from 'pages/TopPage';
+import SignupPage from 'pages/SignupPage';
+import { currentUser, logout } from 'lib/auth';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,12 +18,49 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function () {
+  const [isloggedIn, setIsLoggedIn] = useState(currentUser);
   const classes = useStyles();
+
+  const handleLogout = () => {
+    logout();
+    setIsLoggedIn(null);
+  }
+
+  const PrivateRoute = ({ path, component }) => {
+    return (
+      isloggedIn ? (
+        <Route path={path} component={component} />
+      ) : (
+        <Route
+          render={({ location }) => (
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: location }
+              }}
+            />
+          )}
+        />
+      )
+    );
+  }
 
   return (
     <BrowserRouter>
       <Box className={classes.root}>
         <Switch>
+          <Route path='/signup' render={() =>
+            <SignupPage
+              auth={isloggedIn}
+              setAuth={setIsLoggedIn}
+            />}
+          />
+          {/* <Route path='/login' render={() =>
+            <LoginPage
+              auth={loggedIn}
+              setAuth={setLoggedIn}
+            />}
+          /> */}
           <Route path='/' component={TopPage} />
         </Switch>
       </Box>
