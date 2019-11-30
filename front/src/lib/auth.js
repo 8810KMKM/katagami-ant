6d5d901853d8a71997b704fbd3e3f215f3665c2a
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-11-30 13:22:32 +0900

    Set expiration for auth

--- front/src/lib/auth.js
+++ front/src/lib/auth.js
@@ -1,12 +1,21 @@
+// ログインしたユーザーの情報を時間と一緒に保存
 export const authenticate = user => {
   localStorage.setItem('currentUser', user.id);
   localStorage.setItem('email', user.email);
+  localStorage.setItem('saved', new Date().getTime());
 }
 
-export const isAuthenticated = () => (
-  localStorage.getItem('currentUser')
-);
+// ログイン済み かつ情報が有効期限内であれば真(user.id)
+export const isAuthenticated = () => {
+  const id = localStorage.getItem('currentUser');
+  if (!id || isExpired()) {
+    logout();
+    return null;
+  }
+  return id
+};
 
+// 有効期限内であればユーザー情報を返す
 export const currentUser = () => {
   return ({
     id: localStorage.getItem('currentUser'),
@@ -14,6 +23,18 @@ export const currentUser = () => {
   });
 }
 
+// ログイン情報を強制的にクリア
 export const logout = () => {
   localStorage.clear();
+}
+
+// ログイン情報の有効期限を設定(6時間)
+const isExpired = () => {
+  const expirationHours = 6;
+  const saved = localStorage.getItem('saved');
+  console.log(new Date().getTime() - saved);
+  return (
+    saved &&
+    new Date().getTime() - saved > expirationHours * 60 * 60 * 1000
+  );
 }
