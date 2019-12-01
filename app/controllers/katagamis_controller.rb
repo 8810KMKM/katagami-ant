060eb8435058442936e738cf0f74ab9a0f18a019
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-12-01 23:07:18 +0900

    Change API responce

--- app/controllers/katagamis_controller.rb
+++ app/controllers/katagamis_controller.rb
@@ -1,7 +1,6 @@
 class KatagamisController < ApplicationController
   def index
-    katagamis = Katagami.order(created_at: 'DESC')
-                .select(:id, :src, :width, :height)
-    render json: katagamis
+    katagamis = Katagami.all.select(:id, :src, :width, :height)
+    render json: katagamis.each_slice(2).to_a
   end
 end
