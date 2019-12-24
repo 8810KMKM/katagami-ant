2b3de666fdd00b7e8165b7bc89a5752fa53f4d70
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-12-24 23:05:38 +0900

    Create action add_has_labels

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -13,4 +13,25 @@ class AnnotationsController < ApplicationController
       katagami_height: ant_params[:katagami].height
     }
   end
+
+  def add_has_labels
+    user = User.find(params[:user_id])
+    katagami = Katagami.find(params[:katagami_id])
+    annotation = Annotation.find_by(user: user, katagami: katagami)
+
+    # {has_labels: '1 2 3,3 4,0,...'}
+    params[:has_labels].split(',').each_with_index do |has_label, i|
+      # {has_label: '1 2 3'}
+      next if (has_label[0] == '0')
+      has_label.split(' ').each do |position|
+        HasLabel.create(
+          annotation: annotation,
+          katagami: katagami,
+          label: Label.find(i + 1),
+          position: position.to_i
+        )
+      end
+    end
+    render json: annotation.has_labels
+  end
 end
