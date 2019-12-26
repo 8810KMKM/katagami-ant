bfca8474b2234587ce89de3c818e7620b3634993
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-12-26 10:57:05 +0900

    Fix API

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -16,16 +16,18 @@ class AnnotationsController < ApplicationController
 
   def add_has_labels
     annotation = Annotation.find(params[:annotation_id])
-    # {has_labels: '1 2 3,3 4,0,...'}
-    params[:has_labels].split(',').each_with_index do |has_label, i|
-      # {has_label: '1 2 3'}
-      next if (has_label[0] == '0')
-      has_label.split(' ').each do |position|
+    # {has_labels: '4 1 2 3,2 3 4,1 0'}
+    # {has_labels: 'label.id [positions],label.id [position]'}
+    params[:has_labels].split(',').each do |has_label|
+      _has_label = has_label.split(' ').map { |n| n.to_i }
+      label = Label.find(_has_label.shift)
+
+      _has_label.each do |position|
         HasLabel.create(
           annotation: annotation,
           katagami: annotation.katagami,
-          label: Label.find(i + 1),
-          position: position.to_i
+          label: label,
+          position: position
         )
       end
     end
