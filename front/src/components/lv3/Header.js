6fc12888ddefba757134d6e7cd10a393101f2df1
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-06 18:04:06 +0900

    Display alert on Header while annotation

--- front/src/components/lv3/Header.js
+++ front/src/components/lv3/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Grid, AppBar, Toolbar } from '@material-ui/core'
+import { Grid, AppBar, Toolbar, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import AppLogo from 'components/lv1/AppLogo'
 import UserMenu from 'components/lv2/UserMenu'
+import { useLocation } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,21 +14,30 @@ const useStyles = makeStyles(theme => ({
 export default function(props) {
   const { isLoggedIn, handleLogout } = props
 
+  let location = useLocation()
+  const isAnnotationPage = location.pathname.split('/')[1] === 'ant'
   const classes = useStyles()
 
   return (
     <AppBar postion="static">
       <Toolbar>
-        <Grid container className={classes.root}>
-          <Grid item xs={11}>
-            <AppLogo />
-          </Grid>
-          {isLoggedIn && (
-            <Grid item xs={1}>
-              <UserMenu handleLogout={handleLogout} />
+        {isAnnotationPage ? (
+          <Typography>
+            アノテーション中
+            (ブラウザの「戻るボタン」等でページを離れると実行中のデータは保存されません.)
+          </Typography>
+        ) : (
+          <Grid container className={classes.root}>
+            <Grid item xs={11}>
+              <AppLogo />
             </Grid>
-          )}
-        </Grid>
+            {isLoggedIn && (
+              <Grid item xs={1}>
+                <UserMenu handleLogout={handleLogout} />
+              </Grid>
+            )}
+          </Grid>
+        )}
       </Toolbar>
     </AppBar>
   )
