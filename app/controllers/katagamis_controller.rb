03727ffbd433b4d63c612b6576cafc2cb092b168
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-09 01:32:42 +0900

    Change response of status at katagamis

--- app/controllers/katagamis_controller.rb
+++ app/controllers/katagamis_controller.rb
@@ -63,24 +63,26 @@ class KatagamisController < ApplicationController
       Rails.cache.fetch('katagamis-' + params[:page] + '-' + params[:per]) do
         # A.ログイン中のユーザがアノテーション済みの型紙のidsを取得
         user_done_ids = Katagami.includes(annotations: [:user])
-                                .where(annotations: {status: 2, user_id: params[:user]})
-                                .pluck(:id)
+                                .where(annotations: {user_id: params[:user]})
+                                .pluck(:id, :'annotations.status')
+                                .to_h
 
         # 型紙一覧の情報 アノテーション件数を表示するためinclude
         katagamis = Katagami.includes(:annotations)
                             .page(params[:page]).per(params[:per])
+
 
         # ある型紙 x をログイン中のユーザはアノテーション済みか ?
         # => A のなかに x が入っているか ?
         {
           count: Katagami._count,
           katagamis: katagamis.map {|katagami|
+            status = user_done_ids.find {|k, v| k == katagami.id}
             {
               id: katagami.id,
               name: katagami.name,
               annotation_num: katagami.annotations.size,
-              done_by_current_user: 
-                !!user_done_ids.index(katagami.id)
+              status: status ? status[1] : 0
             }
           }
         }
