9da5a697bb8bb0ff0a6d5f80353128948eb38265
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-13 15:43:14 +0900

    Set hint style

--- front/src/components/lv1/LabelHint.js
+++ front/src/components/lv1/LabelHint.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
-import { Fade, Paper, Typography, IconButton } from '@material-ui/core'
+import { Fade, Paper, Typography, IconButton, Grid } from '@material-ui/core'
 import { Close } from '@material-ui/icons'
+import { grey } from '@material-ui/core/colors'
 
 const useStyles = makeStyles(theme => ({
   root: {
     marginTop: 24,
   },
   content: {
+    backgroundColor: '#eceff1',
     padding: 16,
     height: 274,
   },
+  buttonWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: { padding: 4 },
 }))
 
 export default props => {
@@ -20,10 +27,19 @@ export default props => {
     <div className={classes.root}>
       <Fade in={check}>
         <Paper elevation={2} className={classes.content}>
-          <IconButton onClick={handleClose}>
-            <Close />
-          </IconButton>
-          <Typography>{index}のヒントです.</Typography>
+          <div className={classes.buttonWrapper}>
+            <IconButton onClick={handleClose} className={classes.button}>
+              <Close />
+            </IconButton>
+          </div>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Typography>{index}</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography>{index}のヒントで.</Typography>
+            </Grid>
+          </Grid>
         </Paper>
       </Fade>
     </div>
