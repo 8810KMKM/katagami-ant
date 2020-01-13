7bb316134fedf97bbc9f9b5207ec5b300c0993ef
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-13 17:26:03 +0900

    Create labelInfo and set style

--- front/src/components/lv1/LabelHint.js
+++ front/src/components/lv1/LabelHint.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { makeStyles } from '@material-ui/styles'
 import { Fade, Paper, Typography, IconButton, Grid } from '@material-ui/core'
 import { Close } from '@material-ui/icons'
-import { grey } from '@material-ui/core/colors'
+import { labelInfo } from 'datas/labels'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    marginTop: 24,
-  },
-  content: {
+  root: { marginTop: 24 },
+  contentWrapper: {
     backgroundColor: '#eceff1',
     padding: 16,
-    height: 274,
+    height: 240,
+  },
+  content: { padding: '4px 16px 4px 0' },
+  image: {
+    width: '100%',
+    maxHeight: 163,
   },
   buttonWrapper: {
     display: 'flex',
@@ -23,21 +26,26 @@ const useStyles = makeStyles(theme => ({
 export default props => {
   const { check, index, handleClose } = props
   const classes = useStyles()
+  const hintData = labelInfo[index ? index - 1 : 0]
   return (
     <div className={classes.root}>
       <Fade in={check}>
-        <Paper elevation={2} className={classes.content}>
+        <Paper elevation={2} className={classes.contentWrapper}>
           <div className={classes.buttonWrapper}>
             <IconButton onClick={handleClose} className={classes.button}>
               <Close />
             </IconButton>
           </div>
-          <Grid container spacing={2}>
+          <Grid container spacing={2} className={classes.content}>
             <Grid item xs={6}>
-              <Typography>{index}</Typography>
+              <img
+                className={classes.image}
+                src={hintData.sampleImage}
+                alt={hintData.sampleImage}
+              />
             </Grid>
             <Grid item xs={6}>
-              <Typography>{index}のヒントで.</Typography>
+              <Typography variant="caption">{hintData.description}</Typography>
             </Grid>
           </Grid>
         </Paper>
