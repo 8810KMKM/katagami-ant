c4edce1394269a7e8100cf4cadfd6212cdca5336
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-15 20:34:10 +0900

    Comment out root

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module KatagamiAnt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.time_zone = 'Tokyo'
     config.api_only = true
 
