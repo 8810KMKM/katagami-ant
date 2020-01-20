2962fe0410f11ee1a44db309ec3d19ae9c0757ec
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-20 22:02:16 +0900

    Set font

--- front/src/libs/theme.js
+++ front/src/libs/theme.js
@@ -1,5 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import { jaJP } from '@material-ui/core/locale'
+import KoruriTtf from 'fonts/Koruri-Regular.ttf'
 
 const palette = {
   primary: { main: '#00796B' },
@@ -10,7 +11,22 @@ const palette = {
   success: { main: '#26c6da' },
 }
 
+const koruri = {
+  fontFamily: 'Koruri',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
+    local('Koruri'),
+    local('Koruri-Regular'),
+    url(${KoruriTtf}) format('ttf')
+  `,
+  unicodeRange:
+    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+}
+
 const typography = {
+  fontFamily: 'Koruri, Arial',
   h1: {
     fontSize: 32,
     fontWeight: 700,
@@ -43,4 +59,18 @@ const typography = {
 
 const themeName = 'Hayato OKUMA Katagami annotation tool theme'
 
-export default createMuiTheme({ palette, typography, themeName }, jaJP)
+export default createMuiTheme(
+  {
+    palette,
+    typography,
+    themeName,
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          '@font-face': [koruri],
+        },
+      },
+    },
+  },
+  jaJP
+)
