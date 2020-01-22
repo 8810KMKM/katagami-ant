7c0c0926c5f8d2a494539291001f504d6b35dd22
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-22 23:44:31 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'labels/all'
+  # User
   devise_for :users, controllers: { 
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions'
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'users/sessions#destroy'
   end
-  # User
-  # post '/signup', to: 'users#signup'
-  # post '/login' , to: 'users#login'
-  # get 'users/:id', to: 'users#show'
   # Katagami
   get '/katagamis/:user/:page/:per/:owned_user/:sorting', to: 'katagamis#index'
   get '/katagamis/:id', to: 'katagamis#show'
@@ -18,6 +14,5 @@ Rails.application.routes.draw do
   post '/annotations/:katagami/:user', to: 'annotations#create'
   post '/annotations/add_has_labels', to: 'annotations#add_has_labels'
   # Label
-  get '/labels', to: 'labels#get_random'
   get '/labels/target/:katagami/:user/:num', to: 'labels#target'
 end
