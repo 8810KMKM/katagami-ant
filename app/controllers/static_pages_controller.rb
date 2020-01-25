1ddfe3a7a0450d6a15b3a08f69eb97647688677c
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-25 13:25:23 +0900

    Remove

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ActionController::Base
 
   def index
     if user_signed_in?
-      redirect_to ENV['FRONT_URL'] + current_user.id.to_s
+      redirect_to ENV['FRONT_URL']
     else
       redirect_to '/welcome'
     end
