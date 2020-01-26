360fcfc7f49beb0138aa28523ea0d35a0fda7cdb
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-26 13:16:29 +0900

    JWT使って認証成功！

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,12 +5,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in @user 
-      redirect_to ENV['FRONT_URL']
-      # decode token を url に乗せて送る
-      # redirect_to ENV['FRONT_URL'] + token
+      sign_in @user
+      render json: payload(@user)
+
+      #redirect_to ENV['FRONT_URL'] + auth_payload[:auth_token]
     else
       redirect_to root_path
     end
   end
+
+  private
+    def payload(user)
+      return nil unless user && user.id
+      {
+        auth_token: JsonWebToken.encode({
+          user_id: user.id, 
+          exp: (Time.now + 1.hour).to_i
+        }),
+        user: { id: user.id, email: user.email }
+      }
+    end
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,17 +2,19 @@ class JsonWebToken
   # ブロック内のメソットが全てクラスメソッドになる
   # def self.hogehoge
   class << self
-    def eoncode(payload)
+    def encode(payload)
       JWT.encode(payload, ENV['JWT_SECRET_KEY_BASE'])
     end
 
     def decode(token)
+
+      binding.pry
+      
       HashWithIndifferentAccess.new(
         JWT.decode(token, ENV['JWT_SECRET_KEY_BASE'])
       )[0]
-      rescue
-        nil
-      end
+    rescue
+      nil
     end
   end
 end
