3679c19e665fcb05420d82d561dc7b9aa0f01a6e
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-27 01:42:48 +0900

    Fit for current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_request!
 
   def show
-    render json: User.detail(params[:id])
+    render json: params[:id] ?
+      User.detail(params[:id]) : User.detail(current_user.id)
   end
 end 
