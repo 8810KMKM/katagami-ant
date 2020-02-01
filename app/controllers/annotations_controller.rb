996cf4a488a7e89faf00078fb9fc087f3b016e53
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-02 00:47:50 +0900

    Create action create_with_recommend

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -3,6 +3,13 @@ class AnnotationsController < ApplicationController
     render json: Annotation.stand_by(params[:katagami], current_user.id)
   end
 
+  def create_with_recommend
+    render json: Annotation.stand_by(
+      current_user.recommended_katagami_id,
+      current_user.id
+    )
+  end
+
   def add_has_labels
     render json: 
       Annotation.find(params[:annotation_id]).save_result(params)
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,30 @@ class User < ApplicationRecord
   def self.detail(id)
     Rails.cache.fetch('user-' + id.to_s) do
       user = User.includes(:annotations).find(id)
-      annotations = user.annotations.pluck(:id, :status)
-      doing = annotations.count {|a| a[1].between?(1, 9)}
-      done = annotations.count {|a| a[1] == 10}
+      done, doing = user.annotations.pluck(:id, :status).partition {|a| a[1] == 10}
 
       {
         id: id,
         email: user.email,
         ant_counts: [
-          { status: 'DOING', count: doing },
-          { status: 'DONE',  count: done },
-          { status: 'YET',   count: Katagami.count - (doing + done) }
+          { status: 'DOING', count: doing.size },
+          { status: 'DONE',  count: done.size },
+          { status: 'YET',   count: Katagami.count - (doing.size + done.size) }
         ]
       }
+    end
+  end
+
+  def recommended_katagami_id
+    all_ids = Katagami.all.pluck(:id)
+    doing_or_done_statuses = annotations.pluck(:katagami_id, :status).to_h
+    yet_ids = all_ids - doing_or_done_statuses.keys
+
+    if yet_ids.size > 0
+      yet_ids[0]
+    else
+      recommend = doing_or_done_statuses.sort {|a, b| a[1] <=> b[1]}[0]
+      recommend[1] == 10 ? -1 : recommend[0]
     end
   end
 
