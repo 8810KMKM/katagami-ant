1f04076ee1a3a27f397aba43a03fd3eb4a2b6bb2
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-02 23:56:30 +0900

    Use wrappedId for KatagamiListBody

--- front/src/components/lv2/KatagamiListBody.js
+++ front/src/components/lv2/KatagamiListBody.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { TableRow, TableCell, TableBody, IconButton } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import { Create, Equalizer } from '@material-ui/icons'
@@ -18,8 +19,8 @@ const useStyles = makeStyles(theme => ({
   button: { padding: 4 },
 }))
 
-export default props => {
-  const { katagamis, emptyRows, handleSelectId } = props
+const KatagamiListBody = props => {
+  const { page, rowsPerPage, katagamis, emptyRows, handleSelectId } = props
   const [selectedId, setSelectedId] = useState(0)
   const classes = useStyles()
 
@@ -37,11 +38,13 @@ export default props => {
       </TableCell>
     )
 
+  const wrappedId = i => i + 1 + page * rowsPerPage
+
   return (
     <TableBody>
-      {katagamis.map(katagami => (
+      {katagamis.map((katagami, i) => (
         <TableRow key={katagami.id} className={classes.tableRow}>
-          <TableCell align="right">{katagami.id}</TableCell>
+          <TableCell align="right">{wrappedId(i)}</TableCell>
           <ToggledThumbnail
             katagami={katagami}
             selectedId={selectedId}
@@ -79,3 +82,20 @@ export default props => {
     </TableBody>
   )
 }
+
+KatagamiListBody.propTypes = {
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  katagamis: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      status: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  emptyRows: PropTypes.number,
+  handleSelectId: PropTypes.func.isRequired,
+}
+
+export default KatagamiListBody
