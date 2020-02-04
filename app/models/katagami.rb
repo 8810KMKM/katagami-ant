e196989c11f55c224b4f10d181c03b46732aae5e
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-04 12:51:15 +0900

    Fix

--- app/models/katagami.rb
+++ app/models/katagami.rb
@@ -136,7 +136,7 @@ class Katagami < ApplicationRecord
   end
 
   def plus_ant_num(n=1)
-    update(ant_num: ant_num + n, cw_obj: cw_obj)
+    update(ant_num: ant_num + n, cw_obj: oepn(s3_obj))
   end
 
   # S3バケットに対して認証済みurlを取得
